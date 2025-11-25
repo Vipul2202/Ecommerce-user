564198da4912fdebf8231184a8a55d58c582c305
gurpreetkrai <gurpreetrai1990@gmail.com>
2025-11-25 20:24:12 +0800

    topbar

--- src/assets/Components/TopBar.jsx
+++ src/assets/Components/TopBar.jsx
@@ -26,7 +26,8 @@ const TopBar = () => {
               <div className="leading-tight">
                 <h3 className="text-[#00a0db] font-bold">Opening Hour</h3>
                 <p className="font-bold">Mon – Fri, 7:00 – 5:00</p>
-                <p className="font-bold">Sat–Sun, Appointment Only</p>
+                <p className="font-bold">Sat– 8:00 - 4:00</p>
+                <p className="font-bold">Sun - Appointment Only</p>
               </div>
             </div>
 
